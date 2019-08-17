a5c8bb7d22eb666b5152c7c0dbe8bc6417277c96
2mlcmonkeys <2mlcmonkeys@gmail.com>
2019-08-17 18:02:35 -0400

    deploying heroku working on post and return

--- app/data/friends.js
+++ app/data/friends.js
@@ -16,6 +16,38 @@ var friendsArray = [
             4,
             1
         ]
+    },
+    {
+        name: "Susie Q Lambkins",
+        photo: "https://images.unsplash.com/photo-1535979863199-3c77338429a0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        scores: [
+            2,
+            3,
+            4,
+            5,
+            2,
+            4,
+            3,
+            4,
+            3,
+            1
+        ]
+    },
+    {
+        name: "Amber C",
+        photo: "http://3.bp.blogspot.com/-fGQcKmfH_hY/UgQb8T-A9WI/AAAAAAAAEwI/cT7SWjTiwg4/s1600/7+normal,+hapless.jpg",
+        scores: [
+            3,
+            3,
+            2,
+            3,
+            2,
+            4,
+            1,
+            1,
+            3,
+            2
+        ]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,50 @@
 //Load Data
-var friendData = require("../data/friends");
+var friends = require("../data/friends.js");
 
 //routing
-module.exports = function(app){
+module.exports = function (app) {
     //API GET REQUEST
-    app.get("/api/friends", function(req, res) {
-        res.json(friendData);
-      });
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
 
     //API POST REQUESTS
-    app.post("api/friends", function(req, res){
-        friendData.push(req.body);
-        res.json(true);
-    })
+    app.post("/api/friends", function (req, res) {
+            // newFriend is the user that filled out the survey
+      var newFriend = req.body;
+
+      // compute best match from scores
+      var bestMatch = {};
+      // compare the scores of newFriend with the scores of each friend in the database and find the friend with the smallest difference when each set of scores is compared
+
+      var bestMatchIndex = 0;
+      //greatest score difference for a question is 4, therefore greatest difference is 4 times # of questions in survey
+      var bestMatchDifference = 40;
+
+      for(var i = 0; i < friends.length; i++) {
+        var totalDifference = 0;
+
+        for(var index = 0; index < friends[i].scores.length; index++) {
+          var differenceOneScore = Math.abs(friends[i].scores[index] - newFriend.scores[index]);
+          totalDifference += differenceOneScore;
+        }
+
+        // if the totalDifference in scores is less than the best match so far
+        // save that index and difference
+        if (totalDifference < bestMatchDifference) {
+          bestMatchIndex = i;
+          bestMatchDifference = totalDifference;
+        }
+      }
+
+      // the best match index is used to get the best match data from the friends index
+      bestMatch = friends[bestMatchIndex];
+
+      // Put new friend from survey in "database" array
+      friends.push(newFriend);
+
+      // return the best match friend
+      res.json(bestMatch);
+  });
+    
 }
