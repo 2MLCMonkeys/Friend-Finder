6c5edf5ee6e1112705cf1ea35545f535e4ffa2e9
2mlcmonkeys <2mlcmonkeys@gmail.com>
2020-04-27 12:19:53 -0400

    updating html routes, replace home with index

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,22 +1,22 @@
-//dependencies
+// DEPENDENCIES //
 var path = require('path');
 
-//route
+//  ROUTES //
 module.exports = function(app){
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
       });
 
     app.get("/home", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
       });
     
       app.get("/survey", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
       });
     
-      // If no matching route is found default to home
+      // IF NOT MATHCING ROUTE DEFAULT TO INDEX //
       app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
       });
     };
